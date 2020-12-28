3360eae14701ebeb2df301cdae084fb1de4f72db
pradeeps8 <srinivasa.pradeep@gmail.com>
2020-12-28 14:09:55 -0500

    Update two_sum.py

--- Leet Code/two_sum.py
+++ Leet Code/two_sum.py
@@ -1,14 +1,13 @@
-
 def main():
-  nums = [1,2,5,3,9]
-  print(twoSum(nums, 8)
+    nums = [1,2,5,3,9]
+    print(twoSum(nums, 8))
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-  diffs = {}
-  for i, val in enumerate(nums):
-    if (target-val) in diffs:
-      return [diffs[target-val], i]
-    diffs[val] = i
+def twoSum(nums, target):
+    diffs = {}
+    for i, val in enumerate(nums):
+        if (target-val) in diffs:
+            return [diffs[target-val], i]
+        diffs[val] = i
 
 if __name__ == '__main__':
     main()
